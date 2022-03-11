d1b8641ee1d597be99c13434d2fc1918e73843b9
Jason Rouss <jasonrouss@gmail.com>
2022-03-11 12:15:05 +0200

    Prepare Deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 
@@ -21,4 +21,15 @@ app.use("/v1/api/blood", require("./routes/bloodRoutes"));
 app.use("/v1/api/receive", require("./routes/receiveRoutes"));
 
 app.use("/v1/api/users", require("./routes/userRoutes"));
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"));
+  });
+} else{
+    app.get("/", (req, res) => res.send('Please set to production.'))
+}
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
